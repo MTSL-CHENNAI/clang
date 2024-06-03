25cdf325181318741100d2213a371e1a438d69f2
mtsl-chennai <147499325+MTSL-CHENNAI@users.noreply.github.com>
2024-06-03 15:40:52 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,36 @@
 #include <iostream>
+#include <vector>
 
-void hello_cpp() {
-    std::cout << "Hello, C++ World!" << std::endl;
-}
+class FibonacciGenerator {
+public:
+    FibonacciGenerator(int n) : n_(n) {}
+
+    std::vector<int> generate() {
+        std::vector<int> series;
+        series.reserve(n_);
+        for (int i = 0; i < n_; ++i) {
+            if (i <= 1)
+                series.push_back(i);
+            else
+                series.push_back(series[i - 1] + series[i - 2]);
+        }
+        return series;
+    }
+
+private:
+    int n_;
+};
 
 int main() {
-    hello_cpp();
+    int n = 10;
+    FibonacciGenerator generator(n);
+    std::vector<int> series = generator.generate();
+
+    std::cout << "Fibonacci series up to " << n << " terms:" << std::endl;
+    for (int num : series) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
     return 0;
 }
